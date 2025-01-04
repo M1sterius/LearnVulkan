d86d02944fd2dd91d7315f66ba5321e4e209e4c2
Misterius <kiger3@yandex.ru>
2025-01-05 02:47:33 +0300

    Started following Vulkan Tutorial book

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,20 +1,118 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
-#include "Window.hpp"
-#include "Pipeline.hpp"
-#include "SwapChain.hpp"
-#include "Device.hpp"
-#include "App.hpp"
+#include "Assert.hpp"
 
 #include <cstdlib>
 #include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <filesystem>
+
+#define print(x) std::cout << x << '\n'
+
+class VulkanApp // NOLINT(*-pro-type-member-init)
+{
+public:
+    void Run()
+    {
+        InitWindow();
+        InitVulkan();
+        MainLoop();
+        Cleanup();
+    }
+private:
+    void InitWindow()
+    {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        m_Window = glfwCreateWindow((int)WINDOW_WIDTH, (int)WINDOW_HEIGHT,
+                                    "Learn Vulkan", nullptr, nullptr);
+    }
+
+    void CreateInstance()
+    {
+        // Print all available extensions, comment out if not needed
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        auto extensions = std::vector<VkExtensionProperties>(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount,extensions.data());
+        printf("Extensions available: %u\n", extensionCount);
+        for (const auto& ext : extensions)
+           printf("\t %s \n", ext.extensionName);
+
+        VkApplicationInfo appInfo { };
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Learn Vulkan";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_3;
+
+        VkInstanceCreateInfo createInfo { };
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        // Acquire extensions required for window to work
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        // Validation layers
+        createInfo.enabledLayerCount = 0;
+
+        auto result = vkCreateInstance(&createInfo, nullptr, &m_Instance);
+        if (result != VK_SUCCESS)
+        {
+            throw std::runtime_error("Failed to create Vulkan instance!");
+        }
+    }
+
+    void InitVulkan()
+    {
+        CreateInstance();
+    }
+
+    void MainLoop()
+    {
+        while (!glfwWindowShouldClose(m_Window))
+        {
+            glfwPollEvents();
+        }
+    }
+
+    void Cleanup()
+    {
+        vkDestroyInstance(m_Instance, nullptr);
+        glfwDestroyWindow(m_Window);
+        glfwTerminate();
+    }
+private:
+    const uint32_t WINDOW_WIDTH = 1280;
+    const uint32_t WINDOW_HEIGHT = 720;
+
+    GLFWwindow* m_Window;
+
+    VkInstance m_Instance;
+};
 
 int32_t main(int32_t argc, char* argv[])
 {
-    constexpr glm::uvec2 WINDOW_SIZE = {1280, 720};
+    auto app = VulkanApp();
 
-    auto app = lv::App(WINDOW_SIZE);
-    app.Run();
+    try
+    {
+        app.Run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
 
